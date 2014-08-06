34bad86ae251b9db02a9ef6f9a63a7435ed84b28
Sean Morrison & Blake Mason <student@epicodus.com>
2014-08-06 13:36:31 -0700

    Adds first level menu.

--- train_system_ui.rb
+++ train_system_ui.rb
@@ -1,8 +1,8 @@
 require 'pg'
 require 'pry'
-require 'lines'
-require 'stations'
-require 'stops'
+require './lib/lines'
+require './lib/stations'
+require './lib/stops'
 
 DB = PG.connect({:dbname => 'train_system'})
 
@@ -13,10 +13,42 @@ end
 def header
   system 'clear'
   puts "
-▀▀█▀▀ █▀▀█ █▀▀█ ░▀░ █▀▀▄ 　 ▒█▀▀▀█ █░░█ █▀▀ ▀▀█▀▀ █▀▀ █▀▄▀█
-░▒█░░ █▄▄▀ █▄▄█ ▀█▀ █░░█ 　 ░▀▀▀▄▄ █▄▄█ ▀▀█ ░░█░░ █▀▀ █░▀░█
-░▒█░░ ▀░▀▀ ▀░░▀ ▀▀▀ ▀░░▀ 　 ▒█▄▄▄█ ▄▄▄█ ▀▀▀ ░░▀░░ ▀▀▀ ▀░░░▀
-
+  ╔══╦═╦══╦══╦═╦╗  ╔══╦═╦╦══╦══╦═╦═╦═╗
+  ╚╗╔╣╬║╔╗╠║║╣║║║  ║══╬╗║║══╬╗╔╣╦╣║║║║
+   ║║║╗╣╠╣╠║║╣║║║  ╠══╠╩╗╠══║║║║╩╣║║║║
+   ╚╝╚╩╩╝╚╩══╩╩═╝  ╚══╩══╩══╝╚╝╚═╩╩═╩╝
 "
 end
 
+def main_menu
+  loop do
+    linespace
+    puts "S > Add or view stations."
+    puts "L > Add or view lines."
+    puts "    "
+    puts "X > Exit."
+    linespace
+    main_choice = gets.chomp.upcase
+    if main_choice == "S"
+      station_stuff
+    elsif main_choice == "L"
+      line_stuff
+    elsif main_choice == "X"
+      puts "OK.  Have a good one."
+      linespace
+      exit
+    else
+      system 'clear'
+      puts "Invalid! Try again"
+    end
+  end
+end
+
+def station_stuff
+end
+
+def line_stuff
+end
+
+header
+main_menu
