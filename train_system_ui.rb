e76f4bd4585a2b228770be6508c8eee229092ee6
Sean Morrison & Blake Mason <student@epicodus.com>
2014-08-06 12:18:53 -0700

    Adds header.

--- train_system_ui.rb
+++ train_system_ui.rb
@@ -6,3 +6,17 @@ require 'stops'
 
 DB = PG.connect({:dbname => 'train_system'})
 
+def linespace
+  puts "\n"
+end
+
+def header
+  system 'clear'
+  puts "
+▀▀█▀▀ █▀▀█ █▀▀█ ░▀░ █▀▀▄ 　 ▒█▀▀▀█ █░░█ █▀▀ ▀▀█▀▀ █▀▀ █▀▄▀█
+░▒█░░ █▄▄▀ █▄▄█ ▀█▀ █░░█ 　 ░▀▀▀▄▄ █▄▄█ ▀▀█ ░░█░░ █▀▀ █░▀░█
+░▒█░░ ▀░▀▀ ▀░░▀ ▀▀▀ ▀░░▀ 　 ▒█▄▄▄█ ▄▄▄█ ▀▀▀ ░░▀░░ ▀▀▀ ▀░░░▀
+
+"
+end
+
